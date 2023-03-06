3b15dd16e5475c6899e2269f136860dd5fb606f4
James Jones <jfjones65@gmail.com>
2023-03-05 21:36:18 -0600

    Got it to print to file Too

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import csv
 from csv import reader
 from pathlib import Path
-from typing import List
+from typing import List, TextIO
 
 # gets current file path
 cwd = os.getcwd()
@@ -19,13 +19,8 @@ with open('devices.csv') as csvfile:
         for name in reader.fieldnames:
             d[name].append(row[name])
 
-print()
-
 
 row_count = len(d)
-
-
-
 
 # new "output" dir is created and txt script is created
 Path("Output").mkdir(parents=True, exist_ok=True)
@@ -36,30 +31,27 @@ else:
     new_script = open("Output/linux-dhcp-reservation.txt", "w")
 
 print("===========")
-print("There is " + str(row_count) + " addresses listed in linux-dhcp-reservation.csv\n")
 
 print("Starting script...\n===========")
 
-
-new_script.write("# linux-dhcp-reservation\n\n")
+print("There is " + str(row_count) + " addresses listed in linux-dhcp-reservation.csv\n")
 
 with open('devices.csv') as csvfile:
     # Return a reader object which will
     # iterate over lines in the given csvfile.
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
-        print('host', row[1],'{')
-        print('hardware ethernet',row[0])
-        print('fixed-address', row[2])
-        print('}')
-        print("\n")
+
 
 # with open('devices.csv', 'r') as f:
 #  file = csv.reader(f)
 #  my_list = list(file)
 # print(my_list)
 
+      #  maclist = open("linux-dhcp-reservation.txt", "r")
+      #  read_file = csv.reader(maclist)
         with open('linux-dhcp-reservation.txt', 'w') as f:
+            print("# linux-dhcp-reservation\n\n", file=f)
             for row in readCSV:
                 sys.stdout = f  # Change the standard output to the file we created.
                 print('host', row[1],'{', file=f)
@@ -67,7 +59,6 @@ with open('devices.csv') as csvfile:
                 print('fixed-address', row[2], file=f)
                 print('}',file=f)
                 print("\n", file=f)
+     #   maclist.close()
 
-
-
-print("===========\nScript finished!\n")
+# print("===========\nScript finished!\n")
